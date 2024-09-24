5b9d1624f006b09e10513482d027bfd2b4d579dc
Lars Wikman <lars@underjord.io>
2024-09-24 23:21:29 +0200

    New endpoint for signing more flexible paths

--- lib/fleet_sign_web/controllers/page_controller.ex
+++ lib/fleet_sign_web/controllers/page_controller.ex
@@ -2,6 +2,13 @@ defmodule FleetSignWeb.PageController do
   use FleetSignWeb, :controller
 
   @open_secret "ti2zRfSCr3ITpMU9ReghbGvsy8EOW+VbfAfy18oe59o="
+  def presign(conn, %{"key" => key, "secret" => secret, "serial_number" => serial_number})
+      when secret == @open_secret do
+    path = Path.join("shared", key)
+    {:ok, presigned_url} = Tigris.presign_post(path)
+    json(conn, %{presigned_upload: presigned_url})
+  end
+
   def presign(conn, %{"secret" => secret, "serial_number" => serial_number})
       when secret == @open_secret do
     path = Path.join(["data", serial_number, "location.json"])
